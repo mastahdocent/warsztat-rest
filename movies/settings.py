4d5136e6adde139c883fb022bea34da5a0a04b65
mastahdocent <wojciech@kowasz.pl>
2019-08-31 10:52:59 +0200

    Switching to postgresql

--- movies/settings.py
+++ movies/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "movies.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movies-rest-db',
+        'USER': 'postgres',
+        'PASSWORD': 'p4ssw0rd',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
